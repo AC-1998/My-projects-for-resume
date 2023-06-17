27d4df55d437771bdfca31ca1279984a24d7780d
AC-1998 <acheung024@gmail.com>
2023-06-16 17:16:53 -0700

    changed imports
    changed imports for firebase data.

--- discord-clone/src/firebase.js
+++ discord-clone/src/firebase.js
@@ -1,7 +1,6 @@
-import { initializeApp } from 'firebase/app';
-import { getFirestore } from 'firebase/firestore';
-import { getAuth, GoogleAuthProvider } from 'firebase/auth';
-
+import firebase from "firebase/compat/app";
+import "firebase/compat/auth";
+import "firebase/compat/firestore";
 
 const firebaseConfig = {
   apiKey: "AIzaSyDjEBXi-9oBdWZPBHQBHmZXAHV2Hq-a5FE",
@@ -12,10 +11,10 @@ const firebaseConfig = {
   appId: "1:996990507804:web:eb3fd87a55c298dacab8d9"
 };
 
-const firebaseApp = initializeApp(firebaseConfig);
-const db = getFirestore();
-const auth = getAuth();
-const provider = new GoogleAuthProvider();
+const firebaseApp = firebase.initializeApp(firebaseConfig);
+const db = firebaseApp.firestore();
+const auth = firebase.auth();
+const provider = new firebase.auth.GoogleAuthProvider();
 
 export { auth, provider };
 export default db;
